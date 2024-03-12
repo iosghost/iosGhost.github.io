bf2a0642ff1fa8ed4cc633e2dc1a0c081fb8a198
jesterfile <qlwn20@gmail.com>
2024-03-12 13:41:00 +0300

    init

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt-ftparchive packages ./debs/ > ./Packages;
+#sed -i -e '/^SHA/d' ./Packages;
+bzip2 -c9k ./Packages > ./Packages.bz2;
+printf "Origin: iosghost's Repo\nLabel: iosghost\nSuite: stable\nVersion: 1.0\nCodename: iosghost\nArchitecture: iphoneos-arm\nComponents: main\nDescription: iosghost Best Repo for all Packages! Cydia + Sileo + Installer + Zebra! & Saily.\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+exit 0;
